5bfe5326235790a6b6d96e199e83c3ebf2730e1e
wangjiayan <765390818@qq.com>
2022-08-30 19:19:41 +0800

    实现命令注册,参数解析

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node 
-console.log('我是一个脚手架')
+const lib = require('@wangjiayan-study/test-cli-lib')
+const { argv } = require('node:process')
+
+const command = argv[2]
+const options = argv.slice(3)
+let [option, param] = options
+// 去掉--
+option = option.replace('--',"")
+if (lib[command]){
+    lib[command]({option, param})
+}else{
+    console.log('无效命令')
+}
+
+// 注册一个命令 test
+// 实现一个参数解析
